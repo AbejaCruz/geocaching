c8f1f900123ac74ae0cd770f81f3327fdb9c84df
diegoabeja <abeja.dact_@hotmail.com>
2018-07-22 13:49:23 -0500

    primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+import { LugarPage } from '../pages/lugar/lugar';
+import { TabsPage } from '../pages/tabs/tabs';
+import { PerfilPage } from '../pages/perfil/perfil';
+import { AboutPage } from '../pages/about/about';
+import { TerceraPage } from '../pages/tercera/tercera';
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage,
+    LugarPage,
+    TabsPage,
+    PerfilPage,
+    AboutPage,
+    TerceraPage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage,
+    LugarPage,
+    TabsPage,
+    PerfilPage,
+    AboutPage,
+    TerceraPage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
